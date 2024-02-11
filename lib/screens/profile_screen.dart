65684bd62f582c043b1c7159b54090a36806351d
Panich Sudkhot <pon_7777@hotmail.com>
2024-02-11 15:13:02 +0700

    first commit

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -25,6 +25,85 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/test1x.png'),
+                  radius: 100,
+                ),
+              ),
+              Divider(
+                height: 60,
+                color: Colors.grey[800],
+              ),
+              const Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Keith Chasen',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                'Level',
+                style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                '8',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'keith@mail.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18,
+                      letterSpacing: 1
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+
+
+  
 }
